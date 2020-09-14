f7079de5661fa28788c9cf3bcc669f25743a3581
Rishabh186 <yrishabh94@gmail.com>
2020-09-14 21:59:13 +0530

    Added sample routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'
 import {SignIn}  from './';
+import {Switch,Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-     <SignIn/>
-    </div>
-  );
+function Home(){
+  return(
+   <div>Home</div>    
+  )
 }
+
+function Some(){
+   return (
+     <div>Some</div>
+   )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <Switch>
+         <Route exact path='/' component={SignIn}/>
+         <Route exact path='/home' component={Home}/>
+         <Route exact path='/some' component={Some}/>
+         
+         </Switch>      
+      </div>
+    )
+  }
+}
+
 
 export default App;
